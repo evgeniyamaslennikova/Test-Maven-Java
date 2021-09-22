ebb2f61a472a832fa0e6195bff318e4653d710e5
Evgeniya Maslennikova <evgeniya.maslennikova@sperasoft.com>
2021-09-22 17:52:39 +0300

    Initial Commit

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class DemoTest {
+
+    @Test
+    public void test_Pass() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void test_Fail() {
+        Assert.assertFalse(true);
+    }
+}
